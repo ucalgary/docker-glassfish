1b8b12f2c37abf6a64ee5aea7d5feab92ccf8fc9
King Chung Huang <kinghuang@mac.com>
2016-06-21 19:24:22 -0600

    Change admin password if set
    If the AS_ADMIN_PASSWORD environment variable is set, then change the
    asadmin password before exec-ing the arg.

--- 3.1.2.2/entrypoint.sh
+++ 3.1.2.2/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 if [ "$1" = 'asadmin' ]; then
+	if [ "$AS_ADMIN_PASSWORD" ]; then
+		echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+    echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+
+    # Change the admin password
+    asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+
+    rm /tmp/glassfishpwd
+	fi
+
 	exec "$@"
 fi
 
