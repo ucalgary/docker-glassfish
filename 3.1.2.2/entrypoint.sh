a9e9ceb21942138b3bea97eb0fc908fd798b6d39
King Chung Huang <kinghuang@mac.com>
2016-06-21 19:25:21 -0600

    Enable secure admin
    If the $AS_ADMIN_ENABLE_SECURE environment variable is set, then enable
    secure admin in asadmin.

--- 3.1.2.2/entrypoint.sh
+++ 3.1.2.2/entrypoint.sh
@@ -8,6 +8,13 @@ if [ "$1" = 'asadmin' ]; then
     # Change the admin password
     asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
 
+    if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
+    	echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
+    	asadmin start-domain
+    	asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+    	asadmin stop-domain
+    fi
+
     rm /tmp/glassfishpwd
 	fi
 
