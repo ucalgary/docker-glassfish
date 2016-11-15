f734441609aa2c801828cca49fed2c948e2577e9
King Chung Huang <kinghuang@mac.com>
2016-11-15 12:16:32 -0700

    Normalize spaces and tabs
    Standardize on spaces instead of tabs in this file.

--- 3.1.2.2/docker-entrypoint.sh
+++ 3.1.2.2/docker-entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 if [ "$1" = 'asadmin' ]; then
-	if [ "$AS_ADMIN_PASSWORD" ]; then
-		echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
-    echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+    if [ "$AS_ADMIN_PASSWORD" ]; then
+        echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+        echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
 
-    # Change the admin password
-    asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+        # Change the admin password
+        asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
 
-    if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
-    	echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
-    	asadmin start-domain
-    	asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
-    	asadmin stop-domain
+        if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
+            echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
+            asadmin start-domain
+            asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+            asadmin stop-domain
+        fi
+
+        rm /tmp/glassfishpwd
     fi
 
-    rm /tmp/glassfishpwd
-	fi
-
-	exec "$@"
+    exec "$@"
 fi
 
 exec "$@"
